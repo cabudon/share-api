c901b10b5a989f8b1e27e08b16a2bd68696d4d82
cabudon <cabudon@qq.com>
2023-10-07 21:11:51 +0800

    增加网关模块 gateway 并配置路由转发

--- share-gateway/src/main/java/top/th/share/gateway/config/GatewayApplication.java
+++ share-gateway/src/main/java/top/th/share/gateway/config/GatewayApplication.java
@@ -1,25 +1,25 @@
-package top.th.share.user.config;
+package top.th.share.gateway.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 
 /**
  * @author cabudon
- * @date 2023/10/07 10:25
+ * @date 2023/10/07 19:46
  */
 
 @SpringBootApplication
 @ComponentScan("top.th")
 @Slf4j
-public class UserApplication {
+public class GatewayApplication {
+
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(UserApplication.class);
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
         Environment env = app.run(args).getEnvironment();
-        log.info("启动成功！！");
-        log.info("测试地址：http://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+        log.info("网关启动成功！！");
+        log.info("网关地址：http://127.0.0.1:{}", env.getProperty("server.port"));
     }
 }
