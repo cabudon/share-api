bae15a51377faa68405c6417441f2547de0d04a7
cabudon <cabudon@qq.com>
2023-10-12 19:43:44 +0800

    bug1

---
+++ share-conent/src/main/java/top/th/share/content/config/MyBatisPlusConfig.java
@@ -0,0 +1,22 @@
+package top.th.share.content.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cabudon
+ * @date 2023/10/12 11:16
+ */
+
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor =new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
---
+++ share-conent/src/main/java/top/th/share/content/mapper/ShareMapper.java
@@ -0,0 +1,13 @@
+package top.th.share.content.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import top.th.share.content.domain.entity.Share;
+
+/**
+ * @author cabudon
+ * @date 2023/10/12 14:31
+ */
+
+public interface ShareMapper extends BaseMapper<Share> {
+
+}
