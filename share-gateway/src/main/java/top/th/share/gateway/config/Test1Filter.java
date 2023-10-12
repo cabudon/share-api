9b4b91adc4bca5c6d9169c6765326326e6c29860
cabudon <cabudon@qq.com>
2023-10-12 08:01:35 +0800

    演示 gateway 过滤器的使用

---
+++ share-gateway/src/main/java/top/th/share/gateway/config/Test1Filter.java
@@ -0,0 +1,31 @@
+package top.th.share.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author cabudon
+ * @date 2023/10/11 21:46
+ */
+
+@Component
+@Slf4j
+public class Test1Filter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Test1Filter");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
+
---
+++ share-gateway/src/main/java/top/th/share/gateway/config/Test2Filter.java
@@ -0,0 +1,31 @@
+package top.th.share.gateway.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author cabudon
+ * @date 2023/10/11 21:48
+ */
+
+@Component
+@Slf4j
+public class Test2Filter implements GlobalFilter, Ordered {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Test2Filter");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
